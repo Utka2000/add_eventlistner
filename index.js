c8e2211cb0b2cb24aa8141fa985b8f2a48e77de8
Utka2000 <utkarsh.yashraj@dymensions.io>
2023-06-05 19:18:54 +0530

    Output in console

---
+++ index.js
@@ -0,0 +1,17 @@
+let data = [
+    {
+        player: "Jane",
+        score: 52
+    }, 
+    {
+        player: "Mark",
+        score: 41
+    }
+]
+let log=document.getElementById("log-btn")
+log.addEventListener("click",function(){
+    console.log(data[0].score)
+})
+// Fetch the button from the DOM, store it in a variable
+// Use addEventListener() to listen for button clicks
+// Log Jane's score when the button is clicked (via data)
